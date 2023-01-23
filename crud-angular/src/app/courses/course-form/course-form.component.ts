b3206198836337b6e39f98043384f8fadf044052
Celso Aquino <celso.aquino@ymail.com>
2023-01-22 22:23:42 -0300

    Form: Saving data with HTTP POST + error handling

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,7 @@
+import { CoursesService } from './../services/courses.service';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component( {
   selector: 'app-course-form',
@@ -10,7 +12,10 @@ export class CourseFormComponent {
 
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+    private cursoService: CoursesService,
+    private _snackBar: MatSnackBar
+    ) {
           this.form = this.formBuilder.group( {
             name: [null],
             category: [null]
@@ -19,10 +24,16 @@ export class CourseFormComponent {
   }
 
   onSubmit() {
+    this.cursoService.save(this.form.value)
+    .subscribe(result =>  console.log(result), error => this.onError());
 
   }
 
   onCancel() {
-    
+    console.log("cancel")
+  }
+
+  private onError() {
+    this._snackBar.open('Erro ao salvar curso', '', { duration: 5000 });
   }
 }
