3e8957205039305c2a9a6959d5e28ed9ea6d0986
Celso Aquino <celso.aquino@ymail.com>
2023-01-28 00:43:56 -0300

    Spring API: Fetch by ID

--- crud-spring/src/main/java/com/celsoaquino/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/celsoaquino/crudspring/controller/CourseController.java
@@ -4,22 +4,30 @@ import com.celsoaquino.crudspring.model.Course;
 import com.celsoaquino.crudspring.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
 @AllArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/courses")
 public class CourseController {
 
     private final CourseRepository courseRepository;
 
-    @GetMapping("/courses")
+    @GetMapping()
     public List<Course> list() {
         return courseRepository.findAll();
     }
 
-    @PostMapping("/courses")
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+        return courseRepository.findById(id)
+                .map(redord -> ResponseEntity.ok().body(redord))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping()
     @ResponseStatus(code = HttpStatus.CREATED)
     public Course create(@RequestBody Course course) {
         return courseRepository.save(course);
