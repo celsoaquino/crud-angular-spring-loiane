f247fdd92c7c5ea4dea7e585a02774e8f0c79194
Celso Aquino <celso.aquino@ymail.com>
2023-01-28 18:01:36 -0300

    Front-end Form Validations

--- crud-angular/src/app/courses/conteiners/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/conteiners/course-form/course-form.component.ts
@@ -1,7 +1,7 @@
 import { Course } from './../../model/course';
 import { Location } from '@angular/common';
 import { Component } from '@angular/core';
-import { NonNullableFormBuilder } from '@angular/forms';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 
@@ -15,8 +15,10 @@ import { CoursesService } from '../../services/courses.service';
 export class CourseFormComponent {
   form = this.formBuilder.group({
     _id: [''],
-    name: [''],
-    category: [''],
+    name: ['', [Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(100)]],
+    category: ['', Validators.required],
   });
 
   constructor(
@@ -52,5 +54,29 @@ export class CourseFormComponent {
   private onSuccess() {
     this._snackBar.open('Curso salvo com sucesso!', '', { duration: 5000 });
     this.location.back();
+  }
+
+  getErrorMessage(fieldName: string) {
+    const field = this.form.get(fieldName);
+
+
+
+    if (field?.hasError('required')) {
+      return 'Campo obrigatório';
+    }
+
+    if (field?.hasError('minlength')) {
+
+      const requiredLength = field.errors ? field.errors['minlength']['requiredLength'] : 5 ;
+      return `Tamanho mínimo precisa ser de ${requiredLength} caracteres.`;
+    }
+
+    if (field?.hasError('maxlength')) {
+      console.log(field)
+      const requiredLength = field.errors ? field.errors['maxlength']['requiredLength'] : 200 ;
+      return `Tamanho máximo excedido de ${requiredLength} caracteres.`;
+    }
+
+    return 'Campo Inválido.';
   }
 }
