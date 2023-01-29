b3b1cab564fdb5756f49da3347236c4f217b1820
Celso Aquino <celso.aquino@ymail.com>
2023-01-28 23:18:46 -0300

    Update css

--- crud-spring/src/main/java/com/celsoaquino/crudspring/repository/CourseRepository.java
+++ crud-spring/src/main/java/com/celsoaquino/crudspring/repository/CourseRepository.java
@@ -2,8 +2,6 @@ package com.celsoaquino.crudspring.repository;
 
 import com.celsoaquino.crudspring.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 }
