0172119e9ad93441d0f2fac874e0a0b2664e5d7e
Celso Aquino <celso.aquino@ymail.com>
2023-02-07 23:03:54 -0300

    Standard DTO (Data Transfer Object)

--- crud-spring/src/main/java/com/celsoaquino/crudspring/service/CourseService.java
+++ crud-spring/src/main/java/com/celsoaquino/crudspring/service/CourseService.java
@@ -10,16 +10,14 @@ import jakarta.validation.constraints.Positive;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
-import java.util.Optional;
 
 @Validated
 @Service
 public class CourseService {
 
-    private CourseRepository courseRepository;
+    private final CourseRepository courseRepository;
 
     public CourseService(CourseRepository courseRepository) {
         this.courseRepository = courseRepository;
--- crud-spring/src/test/java/com/celsoaquino/crudspring/CrudSpringApplicationTests.java
+++ crud-spring/src/test/java/com/celsoaquino/crudspring/CrudSpringApplicationTests.java
@@ -1,6 +1,5 @@
 package com.celsoaquino.crudspring;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
