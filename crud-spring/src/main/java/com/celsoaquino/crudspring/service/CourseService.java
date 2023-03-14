17841d23484632d7dde5242961dc3d5137a38676
Celso Aquino <celso.aquino@ymail.com>
2023-03-14 17:01:28 -0300

    Persistence of Enums in JPA (part 2)

--- crud-spring/src/main/java/com/celsoaquino/crudspring/service/CourseService.java
+++ crud-spring/src/main/java/com/celsoaquino/crudspring/service/CourseService.java
@@ -3,7 +3,6 @@ package com.celsoaquino.crudspring.service;
 
 import com.celsoaquino.crudspring.dto.CourseDTO;
 import com.celsoaquino.crudspring.dto.mapper.CourseMapper;
-import com.celsoaquino.crudspring.enums.Category;
 import com.celsoaquino.crudspring.exception.RecordNotFoundException;
 import com.celsoaquino.crudspring.repository.CourseRepository;
 import jakarta.validation.Valid;
@@ -11,7 +10,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -35,7 +33,7 @@ public class CourseService {
                 .collect(Collectors.toList());
     }
 
-    public CourseDTO findById(@PathVariable @NotNull @Positive Long id) {
+    public CourseDTO findById(@NotNull @Positive Long id) {
         return courseRepository.findById(id)
                 .map(courseMapper::toDTO)
                 .orElseThrow(() -> new RecordNotFoundException(id));
@@ -45,11 +43,11 @@ public class CourseService {
         return courseMapper.toDTO(courseRepository.save(courseMapper.toEntity(course)));
     }
 
-    public CourseDTO update(@PathVariable @NotNull @Positive Long id, @Valid @NotNull CourseDTO course) {
+    public CourseDTO update(@NotNull @Positive Long id, @Valid @NotNull CourseDTO course) {
         return courseRepository.findById(id)
                 .map(record -> {
                     record.setName(course.name());
-                    record.setCategory(Category.FRONT_END);
+                    record.setCategory(courseMapper.convertCategoryValue(course.category()));
                     return courseMapper.toDTO(courseRepository.save(record));
                 })
                 .orElseThrow(() -> new RecordNotFoundException(id));
