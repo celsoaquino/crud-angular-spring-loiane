3d2906747f3a8a3303b171e18f2d2722bcb5f6d9
celsoaquino <celso.aquino@ymail.com>
2023-05-10 10:53:26 -0300

    Course-Lesson: Listing

--- crud-spring/src/main/java/com/celsoaquino/crudspring/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/celsoaquino/crudspring/CrudSpringApplication.java
@@ -23,11 +23,18 @@ public class CrudSpringApplication {
             Course c = new Course();
             c.setName("Angular com Spring");
             c.setCategory(Category.FRONT_END);
+
             Lesson l = new Lesson();
             l.setName("Introdução");
             l.setYoutubeUrl("watch?v=1");
             l.setCourse(c);
             c.getLessons().add(l);
+
+            Lesson l1 = new Lesson();
+            l1.setName("Angular");
+            l1.setYoutubeUrl("watch?v=2");
+            l1.setCourse(c);
+            c.getLessons().add(l1);
 
 			courseRepository.save(c);
         };
--- crud-spring/src/main/java/com/celsoaquino/crudspring/dto/CourseDTO.java
+++ crud-spring/src/main/java/com/celsoaquino/crudspring/dto/CourseDTO.java
@@ -1,12 +1,16 @@
 package com.celsoaquino.crudspring.dto;
 
+import com.celsoaquino.crudspring.model.Lesson;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 
+import java.util.List;
+
 public record CourseDTO(
         @JsonProperty("_id") Long id,
         @NotBlank @NotNull @Length(min = 5, max = 100) String name,
-        @NotNull String category) {
+        @NotNull String category,
+        List<Lesson> lessons) {
 }
